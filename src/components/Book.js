30e0320bb83c61bb893e0c401e3c570e4d1f0eed
Nimit Bhargava <nimitbhargava@gmail.com>
2018-02-25 12:46:08 +0530

    Added book component

---
+++ src/components/Book.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function Book(props) {
+    /* object destructuring for cleaner code */
+    const {book} = props
+    return (
+        <li>
+            <div className="book">
+                <div className="book-top">
+                    <div className="book-cover" style={{
+                        width: 128,
+                        height: 193,
+                        backgroundImage: `url(${book.imageLinks.smallThumbnail})`
+                    }}/>
+                    <div className="book-shelf-changer">
+                        <select>
+                            <option value="none" disabled>Move to...</option>
+                            <option value="currentlyReading">Currently Reading</option>
+                            <option value="wantToRead">Want to Read</option>
+                            <option value="read">Read</option>
+                            <option value="none">None</option>
+                        </select>
+                    </div>
+                </div>
+                <div className="book-title">{book.title}</div>
+                <div className="book-authors">{book.authors}</div>
+            </div>
+        </li>
+    )
+}
+
+Book.propTypes = {
+    book: PropTypes.object.isRequired
+}
+export default Book
--- src/components/BooksGrid.js
+++ src/components/BooksGrid.js
@@ -1,33 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Book from './Book'
 
 function BooksGrid(props) {
     return (
         <ol className="books-grid">
             {props.books.length > 0 && (
                 props.books.map((book, index) => (
-                    <li key={index}>
-                        <div className="book">
-                            <div className="book-top">
-                                <div className="book-cover" style={{
-                                    width: 128,
-                                    height: 193,
-                                    backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")'
-                                }}/>
-                                <div className="book-shelf-changer">
-                                    <select>
-                                        <option value="none" disabled>Move to...</option>
-                                        <option value="currentlyReading">Currently Reading</option>
-                                        <option value="wantToRead">Want to Read</option>
-                                        <option value="read">Read</option>
-                                        <option value="none">None</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div className="book-title">To Kill a Mockingbird</div>
-                            <div className="book-authors">Harper Lee</div>
-                        </div>
-                    </li>
+                    <Book book={book} key={index}/>
                 ))
             )}
         </ol>
