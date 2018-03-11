32975143e85fb9d882a55b568b4a3b41a5e45e45
Nimit Bhargava <nimitbhargava@gmail.com>
2018-03-11 17:01:39 +0530

    fix - Books were having self state but not none

--- src/components/BookSearch.js
+++ src/components/BookSearch.js
@@ -22,9 +22,13 @@ class BookSearch extends React.Component {
         }
         BooksAPI.search(query).then((response) => {
             if (Array.isArray(response)) {
+                response = response.map(function (book) {
+                    book.shelf = 'none'
+                    return book
+                })
                 response = response.map(book => {
-                    return Object.assign({}, book, this.props.books.find(shelfData => shelfData.id === book.id));
-                });
+                    return Object.assign({}, book, this.props.books.find(shelfData => shelfData.id === book.id))
+                })
                 this.setState({searchResults: response})
                 return
             }
